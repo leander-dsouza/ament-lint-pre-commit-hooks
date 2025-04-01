811cdc7d520c2fbdabae3527e0f1501e73d54b06
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2025-04-01 21:57:11 +0100

    Fix missing dockerfile (#10)
    * Added abs path for dockerfile.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Bumped up version.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    ---------
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>

--- setup.py
+++ setup.py
@@ -2,4 +2,9 @@ from __future__ import annotations
 
 from setuptools import setup
 
-setup()
+setup(
+    package_data={
+        "ament_lint_pre_commit_hooks": ["Dockerfile"],
+    },
+    include_package_data=True,
+)
